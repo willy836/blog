ed1655f123c4baa0a66a83eef36cfd35a2fa8c6a
DavidManga2546 <132054890+DavidManga2546@users.noreply.github.com>
2023-07-24 12:05:31 +0300

    Add view for creating a post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h2 class="text-center my-4">Single Post Page</h2>
+<form action="{{route('posts.store')}}" method="POST">
+    @csrf
+    <div class="form-group mb-3">
+        <input type="text" name="title" class="form-control" placeholder="Title">
+    </div>
+    <div class="form-group mb-3">
+        <textarea name="content" rows="5" class="form-control" placeholder="Content"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Create Post</button>
+</form>
+
+@endsection
